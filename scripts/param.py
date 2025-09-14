c09a9b61511baa67525a8fa2b2ec79fa7ac54443
hastws <hastws@hotmail.com>
2025-09-14 11:42:25 +0800

    add param.py

---
+++ scripts/param.py
@@ -0,0 +1,15 @@
+"""whether is running on server, on server meaning use GPU with larger memory"""
+
+on_server = False
+# on_server = True
+
+"""The replay path"""
+replay_path = "/Users/hastws/github_work_space/Genesis/replay/"
+
+"""Used in hyperparameter"""
+BATCH_SCALE = 16
+SEQ_SCALE = 16
+SELECT_SCALE = 4
+
+skip_entity_list = False
+skip_autoregressive_embedding = False
